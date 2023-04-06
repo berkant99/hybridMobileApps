a12a5e1ccf228e921edbe4bafdadbba6df3ba63d
BERKANT <berko_99@abv.bg>
2023-04-06 17:46:59 +0300

    First commit

---
+++ OnsenMagic/www/js/index.js
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+function changeBtn(e) {
+    e.setAttribute("modifier", "large")
+}
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    applyDeviceData(device);
+
+    window.addEventListener('batterystatus', onBatteryStatus, false);
+
+    function onBatteryStatus(status) {
+        document.getElementById("battery").value = status.level
+        if (status.level < 30) {
+            ons.notification.toast('Battery low: ' + status.level + "%", { timeout: 2000, animation: 'fall' })
+        }
+    }
+}
+
+function applyDeviceData(device) {
+    document.getElementById('deviceVersion').innerText = device.version;
+    document.getElementById('manufacturer').innerText = device.manufacturer;
+    document.getElementById('deviceModel').innerText = device.model;
+    document.getElementById('operatingSystem').innerText = device.platform;
+}
+
