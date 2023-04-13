00d09754ad4df848d3ba88922062cd62d16c7148
BERKANT <berko_99@abv.bg>
2023-04-13 17:25:32 +0300

    First commit

---
+++ gitcards/src/components/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import '../App.css';
+import * as React from "react";
+import Form from "./Form";
+import CardList from "./CardList";
+
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
+}
+
+export default App;
